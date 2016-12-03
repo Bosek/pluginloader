357377e4ce52dbb915015b8d9ec94ad8bcf7cbf4
Zinal001 <LaNiZ.001@gmail.com>
2016-12-03 23:56:02 +0100

    C# Support + Python Fixes
    *Added support for C# scripts instead of python (plain .cs files)
    *Changed PluginManager so that global variables in Python scripts are
    loaded in the right scope.

---
+++ PluginLoader/PackageManager/PluginClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginLoader
+{
+    public abstract class PluginClass
+    {
+        public readonly PluginGlobal Global;
+        
+        public String PluginDir { get; internal set; }
+
+        public PluginClass(ref PluginGlobal Global)
+        {
+            this.Global = Global;
+        }
+
+        public abstract void Init();
+    }
+}
